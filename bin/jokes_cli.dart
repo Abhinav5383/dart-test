8db91cef28b49db6b2e05c55adb219f8e5ed07dc
Abhinav5383 <itsabhinav5383@gmail.com>
2026-03-12 18:37:45 +0530

    colorful jokes

--- bin/jokes_cli.dart
+++ bin/jokes_cli.dart
@@ -7,33 +7,49 @@ const version = "0.1.0";
 const jokesApiUrl = "https://official-joke-api.appspot.com";
 const magic = "\x1B[1A\x1B[2K";
 
+const _reset = "\x1B[0m";
+const _dim = "\x1B[2m";
+const _yellow = "\x1B[33m";
+const _green = "\x1B[32m";
+
 Future<void> main(List<String> args) async {
   if (args.isNotEmpty && (args.first == "-v" || args.first == "--version")) {
     print("jokes_cli version $version");
     return;
   }
 
-  bool wantAnotherJoke = false;
-
-  do {
+  while (true) {
     await tellJoke();
 
-    print("\nWant another joke? (y/n)");
+    String promptMsg = "${_dim}Want another joke? (Y/n): $_reset";
+    print("");
+    stdout.write(promptMsg);
+
     final String answer = stdin.readLineSync()?.toLowerCase() ?? "";
-    wantAnotherJoke = answer.isEmpty ? false : answer[0] == 'y';
-  } while (wantAnotherJoke == true);
+    if (answer.isNotEmpty && answer[0] == 'n') break;
+
+    stdout.write(magic);
+  }
 }
 
 Future<void> tellJoke() async {
-  String loadingMsg = "\nLoading...";
+  String loadingMsg = "${_dim}Loading joke...$_reset";
   print(loadingMsg);
 
-  Joke joke = await fetchRandomJoke();
-  stdout.write(magic);
-  print(joke.setup.padRight(loadingMsg.length));
-  stdin.readLineSync();
-  stdout.write(magic); // undo the \n written by readLineSync
-  print(joke.punchline);
+  try {
+    Joke joke = await fetchRandomJoke();
+
+    stdout.write(magic);
+    print("$_yellow${joke.setup}$_reset".padRight(loadingMsg.length));
+    stdout.write("$_dim(press Enter for punchline)$_reset");
+    stdin.readLineSync();
+
+    stdout.write(magic);
+    print("$_green${joke.punchline}$_reset");
+  } on Exception catch (e) {
+    stdout.write(magic);
+    print(e);
+  }
 }
 
 typedef Joke = ({String setup, String punchline});
